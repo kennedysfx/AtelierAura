4af4c1b76d7963039ab18515dad28d9760e9192e
kennedy <kennedysezebilo@gmail.com>
2026-07-18 14:39:46 +0100

    feat: added placeholder components to all empty pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+// src/app/contact/page.tsx
+
+export default function ContactPage() {
+  return (
+    <main>
+      <h1>Page Under Construction</h1>
+      {/* Add your contact form or details here */}
+    </main>
+  );
+}
---
+++ src/app/mens/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Page Under Construction</h1>
+    </main>
+  );
+}
