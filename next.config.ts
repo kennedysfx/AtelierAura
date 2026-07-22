4273212d1b515c9a42ccb3174ab0e3b0e8db1c19
kennedy <kennedysezebilo@gmail.com>
2026-07-22 21:37:06 +0100

    go

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    minimumCacheTTL: 31536000,
+  },
 };
 
 export default nextConfig;
